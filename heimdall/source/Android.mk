c82df274ecea32cd6528f070a21528bf80a2f466
Koushik Dutta <koushd@gmail.com>
2013-08-14 09:59:55 -0700

    switch to libusbx
    Change-Id: I91cb1d3fb05c34c9da19d870da3158220628e883

--- heimdall/source/Android.mk
+++ heimdall/source/Android.mk
@@ -3,30 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-  core.c \
-  descriptor.c \
-  io.c \
-  sync.c
-
-ifeq ($(HOST_OS),darwin)
-LOCAL_SRC_FILES += os/darwin_usb.c
-endif
-ifeq ($(HOST_OS),linux)
-LOCAL_SRC_FILES += os/linux_usbfs.c
-endif
-  
-LOCAL_SRC_FILES := $(addprefix ../../../libusb/libusb/,$(LOCAL_SRC_FILES))
-LOCAL_MODULE := libusb
-
-LOCAL_C_INCLUDES := \
-  external/libusb \
-  external/libusb/libusb
-
-include $(BUILD_HOST_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
   Arguments.cpp \
   ClosePcScreenAction.cpp \
   DownloadPitAction.cpp \
@@ -43,13 +19,11 @@ LOCAL_SRC_FILES := \
   main.cpp \
   ../../libpit/Source/libpit.cpp
   
-LOCAL_SRC_FILES += $(LIBUSB_SRC_FILES)
-
 LOCAL_C_INCLUDES := \
   external/heimdall/libpit/Source \
-  external/libusb/libusb
+  external/libusbx/libusb
 
-LOCAL_STATIC_LIBRARIES := libusb
+LOCAL_STATIC_LIBRARIES := libusbx
 
 ifeq ($(HOST_OS),darwin)
 LOCAL_LDFLAGS := -framework CoreFoundation -framework IOKit
